6a0b9fc593a4da446ee3a36f6dec8da5757aec21
rChaoz <catalin_trand2@yahoo.com>
2022-12-05 07:24:19 +0200

    Day 1

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,3 +1,17 @@
-fun day1(data: PuzzleData) = puzzle(data) {
-
+fun day1(data: PuzzleData) = puzzle(data) { input ->
+    // Part 1
+    val elves = buildList {
+        var list = ArrayList<Int>()
+        for (line in input) {
+            if (line.isBlank()) {
+                add(list)
+                list = ArrayList()
+            } else list.add(line.toInt())
+        }
+        add(list)
+    }
+    val totals = elves.map(List<Int>::sum)
+    println(totals.max())
+    // Part 2
+    println(totals.sorted().takeLast(3).sum())
 }
